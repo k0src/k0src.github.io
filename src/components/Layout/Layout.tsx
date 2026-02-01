de0409b2bca9171ca9b62c9eb06cd9dc68118ed2
k0 <k0src@null.net>
2026-02-01 00:33:37 -0600

    scroll to top

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
 import { ProfileCard } from "../ProfileCard/ProfileCard";
 import styles from "./Layout.module.css";
 
 export function Layout() {
+  const mainRef = useRef<HTMLElement>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.scrollTop = 0;
+    }
+  }, [location.pathname]);
+
   return (
     <div className={styles.layout}>
       <Navbar />
       <div className={styles.container}>
         <ProfileCard />
-        <main className={styles.main}>
+        <main ref={mainRef} className={styles.main}>
           <Outlet />
         </main>
       </div>
